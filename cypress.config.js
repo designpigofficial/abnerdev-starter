7fb7f3d999c05f32f1819b5a8984b123647cd8e0
designpigofficial <designpig@outlook.com>
2022-08-16 19:54:48 -0600

    cd: final setup

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8888/',
+    baseUrl: 'https://abnerdevelopment.com',
     supportFile: false,
   },
 });
